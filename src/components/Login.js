0aa956182bd1390a51018761a266c36c411f6345
Neha kumari <kumarineha122162@gmail.com>
2024-12-23 19:08:54 +0530

    build Login Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -96,7 +96,7 @@ const Login = () => {
         <img
           className="bg-gradient-to-b from-black bg-black"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          alt="logo"
+          alt="bg"
         />
       </div>
       <div className="absolute text-white bg-black w-[100%] h-[710px] bg-opacity-60"></div>
